75eb2a0c44c773578a1afffdd78fa8881388408c
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-11-10 09:37:39 +0000

    add o1js

--- cairo/multiply/src/lib.cairo
+++
@@ -1,3 +0,0 @@
-fn main(a : felt252, b : felt252) -> felt252 {
-    a * b
-}
---
+++ o1js/multiply/src/zkProgram.js
@@ -0,0 +1,35 @@
+import { Field, method, verify, Experimental } from "o1js";
+
+const MyProgram = Experimental.ZkProgram({
+  publicInput: Field,
+  publicOutput: Field,
+  methods: {
+    multiply: {
+      privateInputs: [Field],
+      method(a, b) {
+        // This is just to make sure that the fields are read in the correct order
+        a.assertEquals(Field(2));
+        return a.mul(b);
+      },
+    },
+  },
+});
+
+// compile the program
+
+// verificationKey is a special feature found in some ZK systems:
+// it's basically extra (public) data that is shared between Prover and Verifier
+const { verificationKey } = await MyProgram.compile();
+
+// produce proof
+const proof = await MyProgram.multiply(Field(2), Field(3));
+
+// verify proof
+const proofValid = await verify(proof.toJSON(), verificationKey);
+
+console.log("Proof:", JSON.stringify(proof.toJSON()));
+console.log("Public output", proof.publicOutput.value);
+console.log("Public inputs", proof.publicInput.value);
+console.log("Verified successfully:", proofValid);
+
+export { MyProgram };

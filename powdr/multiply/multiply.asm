e3b5e5a8e40e2c02eca619a8acdf1367e6f9032f
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2024-09-15 21:22:15 +0300

    Trying to fix powdr

--- powdr/multiply/multiply.asm
+++ powdr/multiply/multiply.asm
@@ -1,4 +1,4 @@
-machine Multiply with degree: 16 {
+machine Multiply with degree: 8 {
     // this simple machine does not have submachines
 
     reg pc[@pc];
@@ -9,27 +9,18 @@ machine Multiply with degree: 16 {
     reg A;
     reg B;
     reg C;
-    reg D;
 
     // Expose the register value of A in the last time step
-    public N = D(8);
+    public N = C(7);
 
     instr mul X, Y -> Z {
         Z = X * Y
     }
 
-    instr assert_equal X, Y {
-        X = Y
-    }
-
     function main {
         A <=X= ${ std::prelude::Query::Input(0) };
         B <=X= ${ std::prelude::Query::Input(1) };
-        C <=X= ${ std::prelude::Query::Input(2) };
 
-        D <== mul(A, B);
-        assert_equal C, D;
-
-        return;
+        C <== mul(A, B);
     }
 }

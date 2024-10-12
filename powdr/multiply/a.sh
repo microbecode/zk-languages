4595816f0cd897bcc44b92a40c7498639836c288
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2024-10-12 17:03:03 +0300

    try to fix powdr

--- powdr/multiply/a.sh
+++ powdr/multiply/a.sh
@@ -1,4 +1,5 @@
 powdr setup 8 --backend halo2 --field bn254
-powdr pil multiply.asm --field bn254 --force --inputs 2,3
+powdr pil multiply.asm --field bn254 --force --inputs 2
 powdr verification-key multiply.asm --field bn254 --backend halo2 --params params.bin
 powdr prove multiply.asm --field bn254 --backend halo2 --params params.bin --vkey vkey.bin
+powdr verify multiply.asm --field bn254 --backend halo2 --vkey "vkey.bin" --params "params.bin" --proof "multiply_proof.bin" --publics 6
--- powdr/multiply/multiply.asm
+++ powdr/multiply/multiply.asm
@@ -1,26 +1,23 @@
 machine Multiply with degree: 8 {
-    // this simple machine does not have submachines
-
     reg pc[@pc];
     reg X[<=];
     reg Y[<=];
-    reg Z[<=];
-
+    //reg Z[<=];
+    
     reg A;
     reg B;
-    reg C;
 
     // Expose the register value of A in the last time step
-    public N = C(7);
+    public N = B(7);
 
-    instr mul X, Y -> Z {
-        Z = X * Y
+    instr mul X, Y -> Y {
+        Y = X * Y
     }
-
+    
     function main {
-        A <=X= ${ std::prelude::Query::Input(0) };
-        B <=X= ${ std::prelude::Query::Input(1) };
-
-        C <== mul(A, B);
+        A <=X= ${ std::prelude::Query::Input(0, 1) };
+        //B <=X= ${ std::prelude::Query::Input(0, 2) };
+        B <=X= 3;
+        B <== mul(A, B);
     }
 }
